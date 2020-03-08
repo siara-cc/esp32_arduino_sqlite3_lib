1244d9af6d57d788a169863a6382cb992ddbb687
Arun <arun@siara.cc>
2020-03-08 16:23:55 +0530

    Fix memory issues

--- src/config_ext.h
+++ src/config_ext.h
@@ -1,5 +1,4 @@
 #define BUILD_sqlite -DNDEBUG
-#define SQLITE_OMIT_WAL                      1
 #define SQLITE_OMIT_LOAD_EXTENSION           1
 #define SQLITE_DQS                           0
 #define SQLITE_OS_OTHER                      1
@@ -11,25 +10,27 @@
 #define SQLITE_DEFAULT_LOOKASIDE        512,64
 #define YYSTACKDEPTH                        20
 #define SQLITE_SMALL_STACK                   1
+#define SQLITE_DEFAULT_PAGE_SIZE          4096
+#define SQLITE_SORTER_PMASZ                  4
+#define SQLITE_DEFAULT_CACHE_SIZE           -1
+#define SQLITE_DEFAULT_MEMSTATUS             0
+#define SQLITE_DEFAULT_MMAP_SIZE             0
 
 /*
 #define SQLITE_CORE                          1
 #define SQLITE_SYSTEM_MALLOC                 1
 #define SQLITE_THREADSAFE                    0
 #define SQLITE_MUTEX_APPDEF                  1
+#define SQLITE_OMIT_WAL                      1
 #define SQLITE_DISABLE_FTS3_UNICODE          1
 #define SQLITE_DISABLE_FTS4_DEFERRED         1
 #define SQLITE_LIKE_DOESNT_MATCH_BLOBS       1
-#define SQLITE_DEFAULT_CACHE_SIZE           -1
 #define SQLITE_DEFAULT_FOREIGN_KEYS          0
-#define SQLITE_DEFAULT_MEMSTATUS             0
-#define SQLITE_DEFAULT_MMAP_SIZE             0
 #define SQLITE_DEFAULT_LOCKING_MODE          1
 #define SQLITE_DEFAULT_PAGE_SIZE          4096
 #define SQLITE_DEFAULT_PCACHE_INITSZ         8
 #define SQLITE_MAX_DEFAULT_PAGE_SIZE     32768
 #define SQLITE_POWERSAFE_OVERWRITE           1
-#define SQLITE_SORTER_PMASZ                  4
 #define SQLITE_MAX_EXPR_DEPTH                0
 #undef SQLITE_OMIT_ALTERTABLE
 #undef SQLITE_OMIT_ANALYZE
